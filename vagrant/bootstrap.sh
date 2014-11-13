b553cc01066998a7754f8ee2e6e77903d65685ed
Assaf G <assaf@tikalk.com>
2014-11-13 18:54:05 +0200

    added dependencies to bootstrap.sh

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,2 +1,10 @@
 sudo apt-get update -y
 sudo apt-get install git-core nodejs ruby npm -y
+
+
+sudo gem install sass
+
+sudo npm install -g yo@1.1.2
+sudo npm install -g grunt-cli
+sudo npm install -g bower
+sudo npm install -g generator-angular-fullstack
